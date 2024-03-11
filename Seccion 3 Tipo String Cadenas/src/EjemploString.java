6fc771bc793a7b762da321213ca5cd00f6d52bc5
cjrodm22 <cjrodm@outlook.es>
2024-03-11 15:37:22 -0600

    ejemplo String

--- Seccion 3 Tipo String Cadenas/src/EjemploString.java
+++ Seccion 3 Tipo String Cadenas/src/EjemploString.java
@@ -1,6 +1,18 @@
 public class EjemploString {
     public static void main(String[] args) throws Exception {
-      String curso = "Programación Java";
-      
+        String curso = "Programación Java";
+        String curso2 = new String("Progamación java");
+
+        boolean esIgual = curso == curso2;
+
+        System.out.println("esIgual = " + esIgual);
+
+        esIgual = curso.equals(curso2);
+        System.out.println("curso.equals(curso2) = " + esIgual);
+
+        String curso3 = "Programación Java";
+        esIgual = curso == curso3;
+
+        System.out.println("curso = curso3 " + esIgual);
     }
 }
