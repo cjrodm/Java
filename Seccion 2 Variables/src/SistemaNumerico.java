930e80a01d33e507bcc967a7888bb43b4e672fcb
cjrodm22 <cjrodm@outlook.es>
2024-03-11 14:52:10 -0600

    Agegando estructura de control

--- Seccion 2 Variables/src/SistemaNumerico.java
+++ Seccion 2 Variables/src/SistemaNumerico.java
@@ -5,50 +5,63 @@ public class SistemaNumerico {
 
     public static void main(String[] args) {
 
-        int op = 0, numeroDecimal = 0;
+        int seleccion = 0;
 
-        while (op > 4 || op < 1) {
-            System.out.println();
+        while (seleccion != 4) {
+
+            seleccion = menu();
+
+            switch (seleccion) {
+                case 1 -> {
+                    int numeroDecimal = getnumero();
+                    System.out.println("numero Digitado: " + numeroDecimal);
+                    System.out.println("numero binario de: " + numeroDecimal + " : "
+                            + Integer.toBinaryString(numeroDecimal));
+                }
+                case 2 -> {
+                    int numeroDecimal = getnumero();
+                    System.out.println("numero Digitado: " + numeroDecimal);
+                    System.out.println(
+                            "numero Octal de: " + numeroDecimal + " : " + Integer.toOctalString(numeroDecimal));
+                }
+                case 3 -> {
+                    int numeroDecimal = getnumero();
+                    System.out.println("numero Digitado: " + numeroDecimal);
+                    System.out.println(
+                            "numero Hexadecimal de: " + numeroDecimal + " : "
+                                    + Integer.toHexString(numeroDecimal));
+                }
+                case 4 -> System.exit(0);
+                default -> System.out.println("Opción no valida. Intente de nuevo.");
+            }
+        }
+    }
+
+    public static int menu() {
+        int op = 0;
+        while (op <= 0 || op > 4) {
+
+            System.out.println("-----------------");
             System.out.println("\tMenu " +
                     "\n1) Convertir a binario" +
                     "\n2) Convertir a Octal" +
                     "\n3) Convertir a Hexadecimal" +
                     "\n4) Salir");
+            System.out.println("-----------------");
             System.out.print("Digita una opción: ");
+
             try {
                 op = Integer.valueOf(scan.nextLine());
-                if (op == 4)
-                    System.exit(0);
-                if (op >= 1 || op <= 3) {
-                    
-                    System.out.print("Digita el numero a convertir : ");
-                    numeroDecimal = Integer.parseInt(scan.nextLine());
-
-                    switch (op) {
-                        case 1 -> {
-                            System.out.println("numeroDecimal: " + numeroDecimal);
-                            System.out.println(
-                                    "numero binario de: " + numeroDecimal + " : "
-                                            + Integer.toBinaryString(numeroDecimal));
-                        }
-                        case 2 -> {
-                            System.out.println("numeroDecimal: " + numeroDecimal);
-                            System.out.println(
-                                    "numero Octal de: " + numeroDecimal + " : " + Integer.toOctalString(numeroDecimal));
-                        }
-                        case 3 -> {
-                            System.out.println("numeroDecimal: " + numeroDecimal);
-                            System.out.println(
-                                    "numero Hexadecimal de: " + numeroDecimal + " : "
-                                            + Integer.toHexString(numeroDecimal));
-                        }
-                        default -> {
-                        }
-                    }
-                }
             } catch (NumberFormatException e) {
-                System.err.println(e);
+                System.err.println("Error: Debes ingresar un numero");
             }
         }
+        return op;
     }
+
+    public static int getnumero() {
+        System.out.print("Digita el numero a convertir : ");
+        return Integer.parseInt(scan.nextLine());
+    }
+
 }
